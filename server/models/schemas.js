623c03d7eb53e5e3d989cb0f80acdc8b745006d9
Kamil Błoński <kblonski02@gmail.com>
2024-08-12 20:59:39 +0200

    Add UsersModel

--- server/models/schemas.js
+++ server/models/schemas.js
@@ -1,8 +1,11 @@
-import { sequelize } from "../utility/db";
+import { sequelize } from "../utility/db.js";
+import { Users } from "./UsersModel.js";
+
+// TODO - add relations model in this place
 
 await sequelize.sync();
 
 export {
-    
+    Users
 }
 
