f99f16829da7d875fecc4a8304f86ac3ef3ee6d9
Kamil Błoński <kblonski02@gmail.com>
2024-08-12 23:21:35 +0200

    Add relations for models

--- server/models/schemas.js
+++ server/models/schemas.js
@@ -11,7 +11,69 @@ import { Reviews } from "./ReviewsModel.js";
 
 // TODO - add relations model in this place
 
-await sequelize.sync( { force: true} ); //Dev
+// Relation for Address and User
+Address.hasOne(User, {
+    foreignKey: 'address_id'
+});
+User.belongsTo(Address, {
+    foreignKey: 'address_id'
+});
+
+// Relation for products and categories
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+});
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+})
+
+// Relation for Orders and User
+User.hasMany(Order, {
+    foreignKey: 'user_id'
+})
+Order.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Relation for OrdersItem
+Order.hasMany(OrderItems, {
+    foreignKey: 'order_id'
+});
+OrderItems.belongsTo(Order, {
+    foreignKey: 'order_id'
+})
+
+Product.hasMany(OrderItems, {
+    foreignKey: 'product_id'
+})
+OrderItems.belongsTo(Product, {
+    foreignKey: 'product_id'
+})
+
+// Relation for Payments
+Payments.belongsTo(Order, {
+    foreignKey: 'order_id'
+})
+Order.hasOne(Payments, {
+    foreignKey: 'order_id'
+})
+
+// Relation for Reviews
+Reviews.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Reviews, {
+    foreignKey: 'user_id'
+})
+
+Reviews.belongsTo(Product, {
+    foreignKey: 'product_id'
+});
+Product.hasMany(Reviews, {
+    foreignKey: 'product_id'
+})
+
+await sequelize.sync(); //Dev
 
 export {
     User,
