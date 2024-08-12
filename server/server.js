26ce29eb95d63dd222f740b7c63507e0e0096b58
Kamil Błoński <kblonski02@gmail.com>
2024-08-12 17:19:56 +0200

    Create file server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import expressSession from 'express-session';
+
+const app = express();
+
+app.use(express.urlencoded( {extended: false }));
+app.use(expressSession({
+    secret: 'mY_s3creT_C0oD3',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+
+app.get('/', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    const msg = { msg: "Server started"}
+    res.json(msg);
+});
+
+app.listen(3010, ()=>{
+    console.log('Server started at port 3010')
+});
