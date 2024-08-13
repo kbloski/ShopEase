e0d20ab3503e73bb9ced436c1bb569f6f2f42e73
Kamil Błoński <kblonski02@gmail.com>
2024-08-13 16:10:28 +0200

    Add Cors - zezwolenie na korzystanie z zasobów innym domenom

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import expressSession from 'express-session';
+import cors from 'cors';
+import { Json } from 'sequelize/lib/utils';
 
 const app = express();
+
+// CORS (Cross-Origin Resource Sharing) to mechanizm bezpieczeństwa, który kontroluje, które zasoby na serwerze mogą być dostępne z różnych domen, zapobiegając nieautoryzowanemu dostępowi. Działa poprzez dodanie odpowiednich nagłówków HTTP, które umożliwiają lub blokują żądania między różnymi źródłami.
+app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:3000', // lub '*' dla wszystkich źródeł
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+
+
 
 app.use(express.urlencoded( {extended: false }));
 app.use(expressSession({
@@ -19,14 +32,10 @@ app.get('/', (req,res)=>{
     res.json(msg);
 });
 
-
-
-// app.post('/api/login', ( req, res) => {
-//     console.log(req.body)
-// }); 
-
-
-
+app.post('/api/register', (req, res) => {
+    
+    res.send( JSON.stringify( {msg: 'Test połaczenia' } ));
+})
 
 app.listen(3010, ()=>{
     console.log('Server started at port 3010')
