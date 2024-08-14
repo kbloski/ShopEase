c4d78a937620d35ec90f0f833ed7ada4718407e7
Kamil Błoński <kblonski02@gmail.com>
2024-08-15 00:15:39 +0200

    Add simple routing on page

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+import App from './App.js';
 import Register from './pages/Register/Register.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Register />
+    <App />
+
   </React.StrictMode>
 );
 
---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export default class HomePage extends Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>HomePage</h1>
+            </div>
+        )
+    }
+}
