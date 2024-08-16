196c8190c80d4b918b1529a656d546d97cff7d68
Kamil Błoński <kblonski02@gmail.com>
2024-08-16 15:44:53 +0200

    Fix bug in middleware login.js

--- server/middlewares/login.js
+++ server/middlewares/login.js
@@ -2,10 +2,15 @@ import { userController } from "../controllers/controllers.js";
 
 
 const userLogin = async (req, res, next) => {
-    const userDb = await userController.getUserByEmail(req.body.email);
-    const result = await userController.validPassword( req.body.password, userDb);
 
-    if (result) req.user = userDb;
+    try {
+        const userDb = await userController.getUserByEmail(req.body.email);
+        const result = await userController.validPassword( req.body.password, userDb);
+    
+        if (result) req.user = userDb;
+    } catch (err){
+        null;
+    }
     
     next();
 };
