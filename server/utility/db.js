764a0f2edc134c9d8658d8647bdade54b3c2ddfe
Kamil Błoński <kblonski02@gmail.com>
2024-08-21 22:55:39 +0200

    Fix bug with createDatabase

--- server/utility/db.js
+++ server/utility/db.js
@@ -4,21 +4,27 @@ import mysql2 from 'mysql2';
 
 const PORT = 3306;
 const DATABASE = 'shopeasy';
+
+
+// Utwórz połączenie z MySQL w celu obsługi tworzenia bazy danych
 const connection = mysql2.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '',
     port: PORT
 });
-connection.query(`CREATE DATABASE IF NOT EXISTS  CHARACTER SET utf8 COLLATE utf8_general_ci`, (err,result) => {
-    if (err) { 
-        console.log('Baza danych już istnieje: ' + DATABASE)
+
+// Tworzenie bazy danych, jeśli nie istnieje
+connection.query(`CREATE DATABASE IF NOT EXISTS ${DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci`, (err, result) => {
+    if (err) {
+        console.log('Baza danych już istnieje: ' + DATABASE);
     } else {
-        console.log('Utworzono nową bazę danych: ' + DATABASE)
+        console.log('Utworzono nową bazę danych: ' + DATABASE);
     }
 });
-connection.end();
 
+// Zakończ połączenie
+connection.end();
 
 
 const sequelize = new Sequelize({
