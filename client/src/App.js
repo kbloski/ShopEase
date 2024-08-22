ab81d03df4e842f036ed476ae26a644e788cb2a7
Kamil Błoński <kblonski02@gmail.com>
2024-08-22 03:39:07 +0200

    Add Browserouter for all aplications

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,37 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 import storeConfig from './config/storeConfig.js';
 
-import Navigation from './components/Navigation/Navigation.js';
 import HomePage from './pages/HomePage/HomePage.js';
 import Register from './pages/Register/Register.js';
 import Login from './pages/Login/Login.js';
 import ProductAdd from './pages/ProductAdd/ProductAdd.js';
+import ProductView from './pages/ProductView/ProductView.js';
+
 
 function App() {
   const {basicUrl} = storeConfig;
 
   return (
-    <BrowserRouter>
-      <Navigation />
-        <Routes>
-          <Route path= { basicUrl } element={<HomePage />} />
-          <Route path= { basicUrl + '/register'} element={ <Register /> } />
-          <Route path= { basicUrl + '/login'} element={<Login />} />
-          <Route path= { basicUrl + '/product/add'} element={ <ProductAdd />} />
-        </Routes>
-    </BrowserRouter>
+    <div>
+          <Routes>
+            <Route path= { basicUrl } element={<HomePage />} />
+            <Route path= { basicUrl + '/register'} element={ <Register /> } />
+            <Route path= { basicUrl + '/login'} element={<Login />} />
+            <Route path= { basicUrl + '/product/add'} element={ <ProductAdd />} />
+            <Route path= { basicUrl + '/product/view/:id' } element={<ProductView />} />
+          </Routes>
+    </div>
   );
 }
 
-export default App;
+function AppWraper(){
+  // Dodanie navigacji dla wszystkich stron internetowych
+  return (
+    <BrowserRouter>
+      
+      <App />
+    </BrowserRouter>
+  )
+}
+
+export default AppWraper;
