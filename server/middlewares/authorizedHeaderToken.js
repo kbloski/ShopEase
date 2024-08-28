4d1e5e9dc94b3ad4ab99e9aded433f525402f7a4
Kamil Błoński <kblonski02@gmail.com>
2024-08-28 15:42:50 +0200

    Add full data for req.user in authorizedHeaderToken function

--- server/middlewares/authorizedHeaderToken.js
+++ server/middlewares/authorizedHeaderToken.js
@@ -1,20 +1,20 @@
+import { userController } from "../controllers/controllers.js";
 import { webTokenManager } from "../utility/tokenManager.js";
 
-export function authorizeHeaderToken(req, res, next){
+export async  function authorizeHeaderToken(req, res, next){
     const authHeader = req?.headers['authorization'];
 
     if ( authHeader ){
         const token = authHeader.split(" ")[1];
-        
         if (!token) res.status(401).json({msg: '401 Unauthorized', created: false});
         
         const tokenData = webTokenManager.verifyWebToken( token );
-    
-        if ( !tokenData.valid ) res.status( 401 ).json( {msg: '401 Unauthorized'+tokenData.details})
-    
-        req.user = tokenData.decoded.id;
+        
+        const userDb = await userController.getById( tokenData.decoded.id )
+
+        req.user = userDb;
     } else {
-        req.user = {};
+        req.user = null;
     }
 
     next();
