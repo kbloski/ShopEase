84da80b27a65c7bc09395b93d7f386798d1b0429
Kamil Błoński <kblonski02@gmail.com>
2024-09-02 03:12:31 +0200

    Update cors

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import { categoryController, productController, pictureController, reviewController, userController } from './controllers/controllers.js';
-
 import registerRoutes from './index.js';
 import { authorizeHeaderToken } from './middlewares/authorizedHeaderToken.js';
 
 const app = express();
 
-// CORS (Cross-Origin Resource Sharing) to mechanizm bezpieczeństwa, który kontroluje, które zasoby na serwerze mogą być dostępne z różnych domen, zapobiegając nieautoryzowanemu dostępowi. Działa poprzez dodanie odpowiednich nagłówków HTTP, które umożliwiają lub blokują żądania między różnymi źródłami.
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    // lub '*' dla wszystkich źródeł
+/*
+ CORS (Cross-Origin Resource Sharing) to mechanizm bezpieczeństwa, który kontroluje, które zasoby na serwerze mogą być dostępne z różnych domen, zapobiegając nieautoryzowanemu dostępowi. Działa poprzez dodanie odpowiednich nagłówków HTTP, które umożliwiają lub blokują żądania między różnymi źródłami.
+*/
+ app.use(cors({
+    origin: (origin, callback) => {
+        const allowedOrigins = ['http://localhost:3000'];
+        if (allowedOrigins.includes(origin) || !origin){
+            callback( null, true);
+        } else {
+            callback( new Error('Not allowed by CORS'));
+        }
+    } , 
     
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
